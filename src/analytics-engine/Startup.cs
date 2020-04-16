ea1515c5a4584dced747eefb663d406b7a4595b2
Luke Parker <lparkermg@outlook.com>
2020-04-16 12:42:03 +0100

    Fixed counts path

--- src/analytics-engine/Startup.cs
+++ src/analytics-engine/Startup.cs
@@ -26,7 +26,7 @@ namespace analytics_engine
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<ICountService, CountService>(s => new CountService("./counters/"));
+            services.AddSingleton<ICountService, CountService>(s => new CountService("./counts/"));
             services.AddControllers();
         }
 
